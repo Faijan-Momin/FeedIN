e99189d542124e7ae2dc9eeb8c10d2992cf13176
Faijan-Momin <faijanmomin99@gmail.com>
2022-05-15 00:36:53 +0530

    login screen, register as screen

--- app/src/main/java/com/faijan/feedin/Login_activity.java
+++ app/src/main/java/com/faijan/feedin/Login_activity.java
@@ -5,23 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login_activity extends AppCompatActivity {
     ImageView logo;
+    TextView register_user_link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-
-        logo = findViewById(R.id.splash_logo);
-
-
         setContentView( R.layout.activity_login );
 
+        register_user_link = findViewById(R.id.register_user_link);
+
+
+        //        New User Link Click
+        register_user_link.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newUser =  new Intent(getApplication(), Register_As_Activity.class);
+                startActivity( newUser );
+            }
+        } );
 
     }
 }
--- app/src/main/java/com/faijan/feedin/Register_As_Activity.java
+++ app/src/main/java/com/faijan/feedin/Register_As_Activity.java
@@ -2,13 +2,33 @@ package com.faijan.feedin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Register_As_Activity extends AppCompatActivity {
 
+    TextView login_user_link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_register_as );
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+        login_user_link = findViewById(R.id.login_user_link );
+
+
+//        Login User Link Click
+        login_user_link.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registeredUser =  new Intent(getApplication(), Login_activity.class);
+                startActivity( registeredUser );
+            }
+        } );
+
     }
 }
